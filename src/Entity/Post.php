d0405ce783d1040a630035a74f75185dcef76c6f
Inna Nekhai <nekhai.inna@gmail.com>
2018-01-26 11:39:43 +0200

    1

---
+++ src/Entity/Post.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 26.01.2018
+ * Time: 11:15
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="posts")
+ */
+class Post
+{
+
+    private $id;
+    
+    
+}
